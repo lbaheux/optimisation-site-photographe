b7d0a8b6d5ecb40e9fb516d955f5fd3b1c1993d2
Louis Baheux <louis.baheux1@gmail.com>
2026-01-20 15:41:46 +0100

    Corrections : Photo suivante/précédente & Catégorie active dans filterByTag()

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,7 +154,7 @@
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index - 1] || // Correction image précédente dans la modale
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0]; // Correction image suivante dans la modale
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
@@ -241,6 +241,7 @@
       }
       $(".active-tag").removeClass("active active-tag");
       $(this).addClass("active-tag");
+      $(this).addClass("active"); // Correction affichage de la catégorie selectionnée
 
       var tag = $(this).data("images-toggle");
 
